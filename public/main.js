d601eec27f5440336e04b20035921bd228115cf5
ogutkowska <o.gutkowskaz@gmail.com>
2017-04-01 13:59:12 +0200

    update method

---
+++ public/main.js
@@ -0,0 +1,18 @@
+const update = document.getElementById('switch');
+
+update.addEventListener('click', () => {
+  fetch('titles', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'title': "Title has been changed!!",
+      'description': 'Description also has been changed!'
+    })
+  }).then(res => {
+    window.location.reload();
+  }) //wykona sie na pozytytwna odp
+  .catch(err =>{
+    console.log(err);
+  }) //wykona sie na Error
+
+});
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 // 1. One of the things here is to install mongodb and connect to database
+//use tmplate engine to generate html with ajax database
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-
+const BookSchema = require('./bookSchema')
 
 const app = express();
-const port = 3000;
+const port = 8000;
 let db;
 
 //The urlencoded method within body-parser tells body-parser to extract data from the <form> element and add them to the body property in the request object.
 
 app.use(bodyParser.urlencoded({encode: true, extended: true}));
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
 
 MongoClient.connect('mongodb://o.gutkowska:jonaszkofta1@ds147520.mlab.com:47520/book', (err, res) => {
   if(err) {
@@ -24,18 +31,44 @@ MongoClient.connect('mongodb://o.gutkowska:jonaszkofta1@ds147520.mlab.com:47520/
 });
 
 app.get('/', (req, res) => {
-  console.log(__dirname);
-  res.sendFile(__dirname + '/index.html')
+  db.collection('book').find().toArray((err, result) => {
+    res.render('index.ejs', {titles: result});
+    console.log(result);
+  });
+
 });
 
 app.post('/titles', (req, res) => {
-  db.collection('titles')
-    .save(req.body, (err, result) => {
+  const book = new BookSchema;
+  book.title = req.body.title;
+  book.description = req.body.description;
+
+  db.collection('book')
+    .save(book, (err, result) => {
       if (err) {
         return console.log(err)
       }
 
-      console.log('saved to database');
+      console.info('saved to database');
       res.redirect('/')
+  })
+});
+
+app.put('/titles', (req, res) => {
+  db.collection('book').findOneAndUpdate({
+    title: 'Olaolaola'
+  },{
+    $set: {
+      title: req.body.title,
+      description: req.body.description
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, response) => {
+    if(err) {
+      return res.send(err)
+    }
+    res.sent(response);
   })
 });
