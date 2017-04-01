8f986ced79755be58d7d81386628910f0abb7be0
ogutkowska <o.gutkowskaz@gmail.com>
2017-04-01 09:08:50 +0200

    init

---
+++ server.js
@@ -0,0 +1,40 @@
+// 1. One of the things here is to install mongodb and connect to database
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+const port = 3000;
+let db;
+
+//The urlencoded method within body-parser tells body-parser to extract data from the <form> element and add them to the body property in the request object.
+
+app.use(bodyParser.urlencoded({encode: true, extended: true}));
+
+MongoClient.connect('mongodb://kmatyka:qlop01lwe@ds145790.mlab.com:45790/book', (err, res) => {
+  if(err) {
+    return console.error(err);
+  }
+  db = res;
+  app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+  });
+});
+
+app.get('/', (req, res) => {
+  console.log(__dirname);
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/titles', (req, res) => {
+  db.collection('titles')
+    .save(req.body, (err, result) => {
+      if (err) {
+        return console.log(err)
+      }
+
+      console.log('saved to database');
+      res.redirect('/')
+  })
+});
